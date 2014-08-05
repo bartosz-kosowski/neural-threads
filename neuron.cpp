66884742239780c6cc3e4bce13550746b2368a96
Bartosz Kosowski <kosowski.ba@gmail.com>
2014-08-05 12:21:48 +0200

    A simple neuron with sigmoid activation function.

---
+++ neuron.cpp
@@ -0,0 +1,92 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2014  Genkaku <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#include "neuron.h"
+#include <cmath>
+#include <numeric>
+#include <random>
+
+const double activation_sigmoid(const double& summed) {
+  // y=1/(1+e^(-x))
+  return 1.0d / (1.0d + exp(-summed));
+}
+
+
+Neuron::Neuron(size_t number_of_inputs, ActivationFunction function_type)
+  : inputs_num(number_of_inputs)
+{
+  weights.reserve(inputs_num + 1);
+  initalize_weights();
+  
+  set_activation_function(function_type);
+}
+
+Neuron::Neuron(size_t number_of_inputs, ActivationFunction function_type, std::vector<double>& weights)
+  : inputs_num(number_of_inputs)
+{
+  this->weights = weights;
+  
+  set_activation_function(function_type);
+}
+
+
+void Neuron::initalize_weights()
+{
+  //for details see: http://en.cppreference.com/w/cpp/numeric/random
+
+  // Seed with a real random value, if available
+  std::random_device rnd_dev;
+
+  std::default_random_engine random_engine(rnd_dev());
+  std::uniform_real_distribution<double> uniform_dist(-initial_weight_range, initial_weight_range);
+
+  for (size_t windex = 0; windex <= inputs_num; ++windex) {
+    weights.push_back(uniform_dist(random_engine));
+  }
+}
+
+void Neuron::set_activation_function(ActivationFunction function_type)
+{
+  switch (function_type) {
+    case ActivationFunction::SIGMOID:
+      activation_function = activation_sigmoid;
+      break;
+  }
+}
+
+const double Neuron::summation(const std::vector<double> &inputs) {
+  double summed = 0.0d;
+  
+  for (size_t w = 0; w < inputs_num; w++) {
+    summed += weights.at(w) * inputs.at(w);
+  }
+  
+  summed += bias * weights.at(inputs_num);
+  return summed;
+  //return std::accumulate(inputs.begin(), inputs.end(), 0.0d);
+}
+
+const double Neuron::response(const std::vector<double> &inputs) {
+  return activation_function(summation(inputs));
+}
+
+Neuron::~Neuron()
+{
+
+}
---
+++ neuron.h
@@ -0,0 +1,59 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright (C) 2014  Genkaku <email>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef NEURON_H
+#define NEURON_H
+
+#include <vector>
+#include <cstdlib>		// for size_t
+#include <type_traits>		// for add_pointer for function pointer type
+
+using namespace std;
+
+
+using ActivationFunctionPointer_t = add_pointer<const double (const double&)>::type;
+
+enum class ActivationFunction { SIGMOID };
+
+
+class Neuron
+{
+private:
+  size_t inputs_num;
+  vector<double> weights;
+  const double bias = 1.0d;
+  const double initial_weight_range = 0.6d;
+  void initalize_weights();
+  
+  ActivationFunctionPointer_t activation_function;
+  
+  const double summation(const vector<double> &inputs);
+  
+  void _set_weights(vector<double>& weights);
+  
+public:
+  Neuron(size_t number_of_inputs, ActivationFunction function_type);
+  Neuron(size_t number_of_inputs, ActivationFunction function_type, vector<double>& weights);
+  ~Neuron();
+  
+  void set_activation_function(ActivationFunction function_type);
+  const double response(const vector<double> &inputs);
+};
+
+#endif // NEURON_H
